8b971f36b34da04be1388c98d181e92a72d032d6
so-heee <so.heee.mochi@gmail.com>
2021-08-11 10:42:41 +0900

    husky lint-stage動作確認

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-import { hello } from '../test'
-
 export default function Home() {
-  hello('12')
-
   return (
     <div className={styles.container}>
       <Head>
