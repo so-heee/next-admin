3152456321d6dff480f2b5f3baf3170781003e49
so-heee <so.heee.mochi@gmail.com>
2021-08-16 19:13:55 +0900

    テーマのファイルを作成

---
+++ src/styles/theme.tsx
@@ -0,0 +1,19 @@
+import * as colors from '@material-ui/core/colors'
+import { createTheme } from '@material-ui/core/styles'
+
+export const theme = createTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: colors.blue[700],
+    },
+    secondary: {
+      main: colors.blueGrey[800],
+    },
+    background: {
+      default: '#F7F9FC',
+    },
+  },
+})
+
+export default theme
